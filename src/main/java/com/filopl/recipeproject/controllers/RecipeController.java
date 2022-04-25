51da7b8dc7ddcc9ebe43e689c2a10c0ecc3acd3e
FiloPL <tzegarlicki@gmail.com>
2022-04-25 23:11:03 +0200

    add 400 and 404 html class and modify RecipeController

--- src/main/java/com/filopl/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/filopl/recipeproject/controllers/RecipeController.java
@@ -1,14 +1,14 @@
 package com.filopl.recipeproject.controllers;
 
 import com.filopl.recipeproject.commands.RecipeCommand;
+import com.filopl.recipeproject.exceptions.NotFoundException;
 import com.filopl.recipeproject.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 
 /**
@@ -57,5 +57,18 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception) {
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
     }
 }
